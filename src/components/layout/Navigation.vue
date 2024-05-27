4940d662f5ab747613f7176dcd7c4a5e1884c046
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2024-05-27 09:48:28 +0200

    nav

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/"><img src="../../../logo.png"/></RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+
+    <RouterView />
+</template>
+
+<style lang="scss">
+@import "../../assets/scss/settings.scss";
+
+img {
+  width: 20%;
+}
+
+nav {
+    background-color: $primary;
+}
+
+</style>
