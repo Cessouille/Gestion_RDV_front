e32ba24f6f2131bb7276c088006c945ca112f25f
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2024-06-10 08:24:03 +0200

    add colors to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          primary: "7AB2B2",
+          secondary: "#CDE8E5",
+          tertiary: "#4D869C",
+          quartiary: "#EEF7FF",
+        },
+      },
+    },
   },
   plugins: [],
 };
