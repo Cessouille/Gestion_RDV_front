c37e9e9b691397b901739ca5325b750709e8bc90
Yanis Bellahcene <yanis.bellahcene@actemium.com>
2024-06-18 09:26:43 +0200

    - Finished appointment page

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,10 @@ import HistoriqueView from "../views/HistoriqueView.vue";
 import NotificationView from "../views/NotificationView.vue";
 import ProfileView from "../views/ProfileView.vue";
 import LoginView from "../views/LoginView.vue";
-import UserView from "../views/UserView.vue";
+import ProfileView from "../views/UserProfileView.vue";
 import FormTest from "../views/FormTest.vue";
+import AppointmentView from "../views/AppointmentView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
