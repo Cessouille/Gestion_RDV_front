3f5a49ec75330783e1d10e7ab31ef9f037af22f3
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2024-06-18 14:31:16 +0200

    commit

---
+++ src/stores/post.js
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia';
+import { useUserStore } from '@/stores/user';
+
+export const usePostStore = defineStore('post', {
+  state: () => {
+    return { posts: [],
+    connectedUser: useUserStore().me }
+  },
+  actions: {
+    async getPosts() {
+        try {
+            this.posts = [{
+                name: "Jean MALADE",
+                datePubli: new Date("2024/05/27"),
+                content: "Je suis malade et j’ai mal au ventre mais mon cousin ma proposer des huiles essentiels. Que faire ?",
+                nbLike: 50,
+                liked: true,
+              }];
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    },
+    async addPost(post) {
+      try {
+        this.posts.push({
+            name: $cookies.get('userName'),
+            datePubli: new Date(),
+            content: post,
+            nbLike: 0,
+            liked: false,
+        });
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    }
+  },
+});
