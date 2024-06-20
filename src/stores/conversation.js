33ae2d66d0fc96eda420e690dc5034ea5dd27f79
RichardGaming772 <killian.kvella@gmail.com>
2024-06-20 10:28:44 +0200

    added message fetching to conversation vue

--- src/stores/conversation.js
+++ src/stores/conversation.js
@@ -5,13 +5,25 @@ const api = useApiClient();
 
 export const useConversationStore = defineStore('conversation', {
     state: () => {
-        return { conversations: [], error: null }
+        return { conversations: [], error: null, messages: [] }
     },
     actions: {
         async fetchConversations() {
             try {
                 this.conversations = await api.get('/Conversations');
                 this.error = null;
+            } catch (e) {
+                console.error(e);
+                this.error = e;
+                throw e;
+            }
+        },
+        async fetchConversationMessages(userid, convId) {
+            try {
+                var result = await api.get('/Messages/message/' + userid + '/' + convId);
+                this.messages = result.value;
+                this.error = null;
+                return;
             } catch (e) {
                 console.error(e);
                 this.error = e;
